6252585a34acc21de41488552cc0d4bea3cf373c
emrik-hjorth-nti-johanneberg <95085151+emrik-hjorth-nti-johanneberg@users.noreply.github.com>
2023-03-09 11:54:12 +0100

    Jobbat CRUD
    Fixat Edit knapp som directar en till Edit sidan. Fixat en route och action för save knappen på edit sidan, men inte så att den uppdaterar databasen

--- app.rb
+++ app.rb
@@ -19,14 +19,16 @@ post ('/guide/') do
     guide = params[:guide]
     champ = params[:champ]
     db.execute("INSERT INTO Guide (champ_id,title) VALUES (?,?)",champ,guide)
-    redirect("/")
+    id = db.execute("SELECT id FROM Guide ORDER BY id ASC").last()["id"]
+    redirect("/guide/#{id}/edit")
 end
 
 get ('/guide/:id') do
     db = dbConnect()
     id = params[:id].to_i
     @guideContent = db.execute("SELECT * FROM Guide INNER JOIN Champ ON Guide.champ_id = Champ.champ_id WHERE Guide.id = ?",id).first()
-    # @items = db.execute("")
+    @items = db.execute("SELECT Items.* FROM Items INNER JOIN Guide, GuideItemRelation ON Items.item_id = GuideItemRelation.item_id AND Guide.id = GuideItemRelation.guide_id WHERE Guide.id = ?",id)
+    p @items
     slim(:"guides/show")
 end
 
@@ -35,4 +37,23 @@ post ('/guide/:id/delete') do
     id = params[:id].to_i
     result = db.execute("DELETE FROM Guide WHERE id = ?", id)
     redirect('/')
+end
+
+get ('/guide/:id/edit') do
+    db = dbConnect()
+    id = params[:id].to_i
+    @champs = db.execute("SELECT * FROM Champ")
+    @guideContent = db.execute("SELECT * FROM Guide INNER JOIN Champ ON Guide.champ_id = Champ.champ_id WHERE Guide.id = ?",id).first()
+    # @items = db.execute("SELECT Items.* FROM Items INNER JOIN Guide, GuideItemRelation ON Items.item_id = GuideItemRelation.item_id AND Guide.id = GuideItemRelation.guide_id WHERE Guide.id = ?",id)
+    @items = db.execute("SELECT * FROM Items")
+    # p @items
+    slim(:"guides/edit")
+end
+
+post ('/guide/edit') do
+    id = params[:id]
+    # items = params[:]
+
+
+    redirect("guide/#{id}")
 end
